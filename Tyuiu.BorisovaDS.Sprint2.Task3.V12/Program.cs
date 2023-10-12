ace7c6984d33420efbb2f595a8a646cd3a100fde
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-10-12 10:29:30 +0300

    Task3

---
+++ Tyuiu.BorisovaDS.Sprint2.Task3.V12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BorisovaDS.Sprint2.Task3.V12.Lib;
+    
+namespace Tyuiu.BorisovaDS.Sprint2.Task3.V12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Значение функции y = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
