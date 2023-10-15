274075f7a3a11e205eabc63a5ecd57a75cf18dda
Tyuiu.BorisovaDS@outlook.com <yammi253525@gmail.com>
2023-10-15 22:07:30 +0300

    Task6

---
+++ Tyuiu.BorisovaDS.Sprint2.Task6.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BorisovaDS.Sprint2.Task6.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BorisovaDS.Sprint2.Task6.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("14b367bc-3527-4dff-9c9d-0139d84270fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
